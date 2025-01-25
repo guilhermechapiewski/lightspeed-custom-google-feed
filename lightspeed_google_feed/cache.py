36ac069d1bb689d4d24181bccc7cd44d900fc805
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2025-01-24 22:16:51 -0800

    Set default expiration for cached entries.

--- lightspeed_google_feed/cache.py
+++ lightspeed_google_feed/cache.py
@@ -7,15 +7,14 @@ class SimpleCache:
         self.cache = {}
         self.cache_expiration = {}
 
-    def set(self, key, value, time=None):
+    def set(self, key, value, time=3600):
         self.logger.info(f"Set cache: key: {key} | value: {value.__class__.__name__} | time: {time}")
         self.cache[key] = value
-        if time:
-            self.cache_expiration[key] = system_time.time() + time
+        self.cache_expiration[key] = system_time.time() + time
         
     def get(self, key):
-        self.logger.info(f"Getting cache for key: {key}")
         if key in self.cache_expiration and system_time.time() > self.cache_expiration[key]:
+            self.logger.info(f"Cache EXPIRED for key: {key}")
             del self.cache[key]
             del self.cache_expiration[key]
             return None
