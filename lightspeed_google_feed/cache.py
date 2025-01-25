c3c9e9b9dc617dbb7a340edbb90994f81da8e0c5
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2025-01-24 21:58:27 -0800

    Cache usage logs.

--- lightspeed_google_feed/cache.py
+++ lightspeed_google_feed/cache.py
@@ -1,18 +1,30 @@
 import time as system_time
+import logging
 
 class SimpleCache:
     def __init__(self):
+        self.logger = logging.getLogger(__name__)
         self.cache = {}
         self.cache_expiration = {}
 
     def set(self, key, value, time=None):
+        self.logger.info(f"Set cache: key: {key} | value: {value.__class__.__name__} | time: {time}")
         self.cache[key] = value
         if time:
             self.cache_expiration[key] = system_time.time() + time
         
     def get(self, key):
+        self.logger.info(f"Getting cache for key: {key}")
         if key in self.cache_expiration and system_time.time() > self.cache_expiration[key]:
             del self.cache[key]
             del self.cache_expiration[key]
             return None
-        return self.cache.get(key)
+        
+        value = self.cache.get(key)
+
+        if value is None:
+            self.logger.info(f"Cache MISS for key: {key}")
+        else:
+            self.logger.info(f"Cache HIT for key: {key}")
+
+        return value
